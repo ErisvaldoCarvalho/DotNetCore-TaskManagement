38c5bfd9a581f8bfda799830b08f4bb6fc7d3d1e
erisvaldocarvalho <erisvaldocarvalhosilva@gmail.com>
2021-12-22 15:24:26 -0300

    Bugado
    Carregando a solução...

--- WebApp.TaskManagement/Startup.cs
+++ WebApp.TaskManagement/Startup.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApp.TaskManagement
 {
@@ -23,6 +21,22 @@ namespace WebApp.TaskManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            
+            services.AddSwaggerGen(C =>
+            {
+                C.SwaggerDoc("V1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Task Management",
+                    Version = "v1",
+                    Description = "Projeto de gestão de tarefas",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    {
+                        Name = "Erisvaldo Carvalho",
+                        Email = "contato@erisvaldocarvalhosilva.com.br",
+                        Url = new Uri("https://github.com/ErisvaldoCarvalho")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,11 +55,29 @@ namespace WebApp.TaskManagement
 
             app.UseRouting();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Task Management");
+            });
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
+
             app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapRazorPages();
+            //});
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                //endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute("DefaultApi", "{controller=values}/{id?}");
             });
         }
     }
